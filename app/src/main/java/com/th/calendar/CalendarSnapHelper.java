433b9f2588a0db3f0e35e36aa6687de7ba9bafd8
th <thai.huynh.xyz@gmail.com>
2017-08-04 00:29:38 +0700

    update linearSmoothScroller

--- app/src/main/java/com/th/calendar/CalendarSnapHelper.java
+++ app/src/main/java/com/th/calendar/CalendarSnapHelper.java
@@ -3,10 +3,12 @@ package com.th.calendar;
 import android.graphics.PointF;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearSnapHelper;
+import android.support.v7.widget.LinearSmoothScroller;
 import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.PagerSnapHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SnapHelper;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 /**
@@ -21,8 +23,10 @@ import android.view.View;
  * {@link android.view.ViewGroup.LayoutParams#MATCH_PARENT} height and width and then attach
  * CalendarSnapHelper to the {@link RecyclerView} using {@link #attachToRecyclerView(RecyclerView)}.
  */
-class CalendarSnapHelper extends LinearSnapHelper {
+class CalendarSnapHelper extends PagerSnapHelper {
 
+    private static final int MAX_SCROLL_ON_FLING_DURATION = 100; // ms
+    private static final float MILLISECONDS_PER_INCH = 100.0F;
     static final int ITEM_PER_MONTH = 49;
 
     // Orientation helpers are lazily created per LayoutManager.
@@ -82,6 +86,70 @@ class CalendarSnapHelper extends LinearSnapHelper {
         return mSnapPosition;
     }
 
+    @Override
+    protected LinearSmoothScroller createSnapScroller(final RecyclerView.LayoutManager layoutManager) {
+        if (!(layoutManager instanceof RecyclerView.SmoothScroller.ScrollVectorProvider)) {
+            return null;
+        }
+        return new LinearSmoothScroller(mRecyclerView.getContext()) {
+
+            // Trigger a scroll to a further distance than TARGET_SEEK_SCROLL_DISTANCE_PX so that if target
+            // view is not laid out until interim target position is reached, we can detect the case before
+            // scrolling slows down and reschedule another interim target scroll
+            private static final float TARGET_SEEK_EXTRA_SCROLL_RATIO = 1.2f;
+
+            @Override
+            protected void onTargetFound(View targetView, RecyclerView.State state, Action action) {
+                int[] snapDistances = calculateDistanceToFinalSnap(mRecyclerView.getLayoutManager(),
+                        targetView);
+                assert snapDistances != null;
+                final int dx = snapDistances[0];
+                final int dy = snapDistances[1];
+                final int time = calculateTimeForDeceleration(Math.max(Math.abs(dx), Math.abs(dy)));
+                if (time > 0) {
+                    action.update(dx, dy, time, mDecelerateInterpolator);
+                }
+            }
+
+            @Override
+            protected float calculateSpeedPerPixel(DisplayMetrics displayMetrics) {
+                return MILLISECONDS_PER_INCH / displayMetrics.densityDpi;
+            }
+
+            @Override
+            protected int calculateTimeForScrolling(int dx) {
+                return Math.min(MAX_SCROLL_ON_FLING_DURATION, super.calculateTimeForScrolling(dx));
+            }
+
+            @Override
+            protected void updateActionForInterimTarget(Action action) {
+                // find an interim target position
+                PointF scrollVector = computeScrollVectorForPosition(getTargetPosition());
+                if (scrollVector == null || (scrollVector.x == 0 && scrollVector.y == 0)) {
+                    final int target = getTargetPosition();
+                    action.jumpTo(target);
+                    stop();
+                    return;
+                }
+                normalize(scrollVector);
+                mTargetVector = scrollVector;
+
+                final View centerView = findCenterView(layoutManager, getHorizontalHelper(layoutManager));
+                assert centerView != null;
+                final int targetSeekScrollDistancePx = (int) (mRecyclerView.getWidth() - centerView.getX() - centerView.getWidth() / 2);
+                mInterimTargetDx = (int) (targetSeekScrollDistancePx * scrollVector.x);
+                mInterimTargetDy = (int) (targetSeekScrollDistancePx * scrollVector.y);
+                final int time = calculateTimeForScrolling(targetSeekScrollDistancePx);
+                // To avoid UI hiccups, trigger a smooth scroll to a distance little further than the
+                // interim target. Since we track the distance travelled in onSeekTargetStep callback, it
+                // won't actually scroll more than what we need.
+                action.update((int) (mInterimTargetDx * TARGET_SEEK_EXTRA_SCROLL_RATIO),
+                        (int) (mInterimTargetDy * TARGET_SEEK_EXTRA_SCROLL_RATIO),
+                        (int) (time * TARGET_SEEK_EXTRA_SCROLL_RATIO), mLinearInterpolator);
+            }
+        };
+    }
+
     /**
      * Return the child view that is currently closest to the center of this parent.
      *
@@ -109,7 +177,11 @@ class CalendarSnapHelper extends LinearSnapHelper {
 
         for (int i = 0; i < childCount; i++) {
             final View child = layoutManager.getChildAt(i);
-            if (layoutManager.getPosition(child) % ITEM_PER_MONTH != ITEM_PER_MONTH / 2) continue;
+            final int childMonthIndex = layoutManager.getPosition(child) % ITEM_PER_MONTH;
+            if (childMonthIndex != ITEM_PER_MONTH / 2) {
+                i += ITEM_PER_MONTH * (childMonthIndex > ITEM_PER_MONTH / 2 ? 1.5f : 0.5f) - childMonthIndex - 1;
+                continue;
+            }
             int childCenter = helper.getDecoratedStart(child)
                     + (helper.getDecoratedMeasurement(child) / 2);
             int absDistance = Math.abs(childCenter - center);
@@ -174,12 +246,49 @@ class CalendarSnapHelper extends LinearSnapHelper {
     }
 
     void next() {
-        mSnapPosition -= ITEM_PER_MONTH;
+        mSnapPosition -= ITEM_PER_MONTH * (mSnapPosition % ITEM_PER_MONTH == 0 ? 0.5 : 1);
+        mRecyclerView.smoothScrollToPosition(mSnapPosition);
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            boolean mScrolled = false;
+
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == 0 && this.mScrolled) {
+                    mRecyclerView.removeOnScrollListener(this);
+                    mRecyclerView.scrollToPosition(mSnapPosition);
+                }
+            }
+
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                if (dx != 0 || dy != 0) {
+                    this.mScrolled = true;
+                }
+            }
+        });
         mRecyclerView.smoothScrollToPosition(mSnapPosition);
     }
 
     void prev() {
-        mSnapPosition += ITEM_PER_MONTH;
+        mSnapPosition += ITEM_PER_MONTH * (mSnapPosition % ITEM_PER_MONTH == 0 ? 1.5 : 1);
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            boolean mScrolled = false;
+
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == 0 && this.mScrolled) {
+                    mRecyclerView.removeOnScrollListener(this);
+                    mRecyclerView.scrollToPosition(mSnapPosition);
+                }
+            }
+
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                if (dx != 0 || dy != 0) {
+                    this.mScrolled = true;
+                }
+            }
+        });
         mRecyclerView.smoothScrollToPosition(mSnapPosition);
     }
 
